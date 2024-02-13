329481c76b8f0ae3d175e80cc4b7f1a197ee649e
URP\유알피 <d>
2024-02-13 14:36:46 +0900

    fetch data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,12 +2,57 @@ export const metadata = {
     title: "Home",
 };
 
-export default function Page(){
+//nomad-movies.nomadcoders.workers.dev
+//movies/[id]/videos
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+export default async function HomePage(){
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    
+    const movies = await getMovies();
+
     return(
         <>
-            <h1>
-                asd
-            </h1>
+            {JSON.stringify(movies)}
         </>
     )
 }
+//next fetches in the backend
+async function getMovies(){
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+
+/*
+    react fetch
+*/
+/*
+    export default function Page(){
+
+        const [isLoading, setIsLoading] = useState(true);
+
+        const [movies, setMovies] = useState([]);
+
+        const getMovies = async () => {
+            const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+    
+            const json = await response.json();
+    
+            setMovies(json);
+            setIsLoading(false);
+        }
+    
+        useEffect(()=>{
+            getMovies();
+        },[]);
+    
+        return(
+            <>
+                {isLoading ? "Loading" : JSON.stringify(movies)}
+            </>
+        )
+    }
+
+*/
