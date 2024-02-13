10ace9940e068ce9ebbccf168b1f46cb6cc38046
URP\유알피 <d>
2024-02-13 11:18:24 +0900

    first commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "Home",
+};
+
+export default function Page(){
+    return(
+        <>
+            <h1>
+                asd
+            </h1>
+        </>
+    )
+}
