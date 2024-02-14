f4fbac05797b1d1c7930a1ab2eed79c48178d527
URP\유알피 <d>
2024-02-14 17:51:49 +0900

    movies, videos parallel

---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading(){
+  return(
+    <>
+      <h2>
+        Loading a movie :id
+      </h2>
+    </>
+  )
+}
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,34 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page"
+
+export default async function MovieDetail({
     params: {id}
 }:{
     params: {id: string}
 }){
+    console.log('Start Fetch');
+    //const movie = await getMovie(id);
+    //const videos = await getVideos(id);
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+    console.log('End Fetch');
 
     return(
-        <h1>Movie {id}</h1>
+        <h1>{movie.title}</h1>
     )
+}
+
+async function getMovie(id:string){
+    console.log(`Fetching movie: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response = await fetch(`${API_URL}/${id}`);
+
+    return response.json();
+}
+
+async function getVideos(id:string){
+    console.log(`Fetching videos: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response = await fetch(`${API_URL}/${id}/videos`);
+
+    return response.json();
 }
